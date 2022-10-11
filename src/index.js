22569701f0f97b8399a00c6b359e990a58fda47b
dinhhuutai <dinhhuutai20023107@gmail.com>
2022-10-11 11:51:46 +0700

    Trick rename SortMiddleware to sortMiddleware

--- src/index.js
+++ src/index.js
@@ -1,33 +1,79 @@
 const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
+const methodOverride = require('method-override');
 const { engine } = require("express-handlebars");
+
+const SortMiddleware = require('./app/middlewares/SortMiddleware');
+
 const app = express();
 const port = 3000;
 
-
 const route = require("./routes");
+const db = require("./config/db");
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Connect to DB
+db.connect();
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // Middleware
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 app.use(express.json());
+
+app.use(methodOverride('_method'));
+
+// Custom middleware
+app.use(SortMiddleware);
 
 // HTTP logger
 app.use(morgan("combined"));
 
 // Template engine
-app.engine("hbs", engine({extname: ".hbs"}));
+app.engine(
+    "hbs",
+    engine({
+        extname: ".hbs",
+        helpers: {
+            sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+
+                const sortType = field === sort.column ? sort.type : 'default';
+
+                const isons = {
+                    default: 'fa-solid fa-sort',
+                    asc: 'fa-solid fa-arrow-down-short-wide',
+                    desc: 'fa-solid fa-arrow-down-wide-short',
+                };
+
+                const types ={
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc',
+                };
+
+                const icon = isons[sortType];
+                const type = types[sortType];
+
+                return `
+                    <a href="?_sort&column=${field}&type=${type}">
+                        <i class="${icon}"></i>
+                    </a>
+                `
+            }
+        }
+    })
+);
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, 'resources/views'));
+app.set("views", path.join(__dirname, "resources", "views"));
 
 // Routes init
 route(app);
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
